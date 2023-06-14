d542a3f3c809712e3bc558e48fe1ebb0319beaf3
adosilva <abimael.silva@sp.senai.br>
2023-06-14 19:09:18 -0300

    update prj discovery space rev.21 > 14/06/2023

---
+++ js/prod.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function () {
+  jQuery(".ecommerce-gallery").lightSlider({
+    gallery: true,
+    item: 1,
+    loop: true,
+    thumbItem: 4,
+    thumbMargin: 10,
+  });
+});
